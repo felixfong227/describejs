a63f29c9f86f2581a7ceaa0950897cb5c46c26a0
Felix Fong <moongod101@hotmail.com>
2017-06-19 17:18:20 +0800

    Add some color to the file name and expect type

--- src/describe.js
+++ src/describe.js
@@ -6,6 +6,8 @@ const readDir = require('fs-readdir-recursive');
 
 const cwd = process.cwd();
 
+const color = require('cli-color');
+
 // Read the package.json file
 const packageJSON = JSON.parse(fs.readFileSync(path.join(`${__dirname}/../package.json`), 'utf-8'));
 cli
@@ -22,13 +24,20 @@ function coreCheck(content, fileName) {
         const describeSyntax = new RegExp(/\/\*([\s\S]*?)\*\//igm);
         if (content.match(describeSyntax)) {
             // Get the describe context
-            console.log(`\n${fileName}:`);
+            console.log(color.blue(`\n${fileName}:`));
             // Get the context
             const context = content.match(describeSyntax);
             context.forEach((describeSyntaxContext) => {
                 // Remove 2(tab) or 4(tab) space characters
                 describeSyntaxContext = describeSyntaxContext.replace(/ {2}| {4}/igm, '');
                 console.log(`\n${describeSyntaxContext}\n`);
+
+                // Check for expecting return type => @return <type> # type is expect 1 to 8 characters
+                const expectType = new RegExp(/@return.[a-z]{1,8}/i);
+                if (describeSyntaxContext.match(expectType)) {
+                    const returnType = describeSyntaxContext.match(expectType)[0].replace('@return ', '');
+                    console.log(color.yellow(`Expect to return ${returnType}`));
+                }
             });
         } else {
             throw new Error(`${path.join(`${lookUpPath}/${fileName}/ `)}do not contain describe syntax`);
